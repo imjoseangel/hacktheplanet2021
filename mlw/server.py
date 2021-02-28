3d66abcdc71264707e4af295a8985042667bb43e
imjoseangel <josea.munoz@gmail.com>
2021-02-28 12:16:52 +0100

    Add monitorp

--- mlw/server.py
+++ mlw/server.py
@@ -1,11 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 """
 Main module of the server file
 """
+
+from os.path import abspath, dirname, normpath
+import sys
 from flask import render_template
+import hupper
 from waitress import serve
 
 # local modules
 import config
+
+INDEX_FILE = 'index.html'
+MAP_FILE = 'locations.html'
+DB_FILE = 'mlw.db'
+
+# Set local path
+here = normpath(abspath(dirname(__file__)))
 
 # Get the application instance
 connex_app = config.connex_app
@@ -32,6 +45,18 @@ def locations():
     return render_template('locations.html')
 
 
-if __name__ == "__main__":
+def main():
+
+    # start_reloader will only return in a monitored subprocess
+    reloader = hupper.start_reloader('server.main')
+
+    # monitor an extra file
+    reloader.watch_files(
+        [f'{here}/templates/{MAP_FILE}', f'{here}/templates/{INDEX_FILE}', f'{here}/{DB_FILE}'])
+
     # connex_app.run(debug=True)
     serve(connex_app.app)
+
+
+if __name__ == '__main__':
+    main()
